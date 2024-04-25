14154d3b8c4390b1a6e1c70497d64a74fcd9b9a4
mehedi-softdev <mehedi.softdev@gmail.com>
2024-04-26 01:18:51 +0600

    photo gallery screen design done

--- lib/pages/gallery_screen.dart
+++ lib/pages/gallery_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:photo_gallery/models/dummy_data.dart';
+import 'package:photo_gallery/utils/custom_color.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
@@ -7,17 +10,51 @@ class GalleryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        children: List.generate(DummyData.getImages().length, (index) {
-          return GestureDetector(
-            onTap: () {
-
-            },
-            child: Image.asset(DummyData.getImages()[index].img),
-          );
-        }),
+      crossAxisCount: 2,
+      mainAxisSpacing: 5,
+      crossAxisSpacing: 5,
+      padding: const EdgeInsets.all(5.0),
+      children: List.generate(DummyData.getImages().length, (index) {
+        return GestureDetector(
+          onTap: () {},
+          child: Card(
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            shadowColor: Color(CustomColor.charcoalGrey.value),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image.asset(
+                    DummyData.getImages()[index].img,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+                // for title text
+                Positioned(
+                    bottom: 10,
+                    left: 10,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        DummyData.getImages()[index].title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
