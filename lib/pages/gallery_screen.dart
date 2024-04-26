9e14a638f0766f7d14306a937fed93423aba4960
mehedi-softdev <mehedi.softdev@gmail.com>
2024-04-26 17:36:02 +0600

    based on orientation responsiveness added

--- lib/pages/gallery_screen.dart
+++ lib/pages/gallery_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:photo_gallery/models/dummy_data.dart';
-import 'package:photo_gallery/models/gallery.dart';
 import 'package:photo_gallery/utils/custom_color.dart';
 import 'package:photo_gallery/utils/routes.dart';
 
@@ -10,53 +9,106 @@ class GalleryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      mainAxisSpacing: 5,
-      crossAxisSpacing: 5,
-      padding: const EdgeInsets.all(5.0),
-      children: List.generate(DummyData.getImages().length, (index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, Routes.detail.route,
-                arguments: DummyData.getImages()[index]);
-          },
-          child: Card(
-            elevation: 8.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            shadowColor: Color(CustomColor.charcoalGrey.value),
-            child: Stack(
-              children: [
-                ClipRRect(
+    return OrientationBuilder(builder: (context, orientation) {
+      if(orientation == Orientation.portrait) {
+        return GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          padding: const EdgeInsets.all(5.0),
+          children: List.generate(DummyData.getImages().length, (index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, Routes.detail.route,
+                    arguments: DummyData.getImages()[index]);
+              },
+              child: Card(
+                elevation: 8.0,
+                shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0),
-                  child: Image.asset(
-                    DummyData.getImages()[index].img,
-                    fit: BoxFit.cover,
-                  ),
                 ),
-                // for title text
-                Positioned(
-                    bottom: 10,
-                    left: 10,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text(
-                        DummyData.getImages()[index].title,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          fontFamily: 'Roboto',
-                        ),
+                shadowColor: Color(CustomColor.charcoalGrey.value),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Image.asset(
+                        DummyData.getImages()[index].img,
+                        fit: BoxFit.cover,
                       ),
-                    ))
-              ],
-            ),
-          ),
+                    ),
+                    // for title text
+                    Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            DummyData.getImages()[index].title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            );
+          }),
         );
-      }),
-    );
+      }else {
+        return GridView.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          padding: const EdgeInsets.all(5.0),
+          children: List.generate(DummyData.getImages().length, (index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, Routes.detail.route,
+                    arguments: DummyData.getImages()[index]);
+              },
+              child: Card(
+                elevation: 8.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                shadowColor: Color(CustomColor.charcoalGrey.value),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Image.asset(
+                        DummyData.getImages()[index].img,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    // for title text
+                    Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            DummyData.getImages()[index].title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            );
+          }),
+        );
+      }
+    },);
   }
 }
